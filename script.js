de25d9e729a37eab06fc218c1ca2a7365e045eff
Joseph Sumis <Jsumis@comcast.net>
2021-04-21 23:21:31 -0500

    created index, css, and js files, copied over given code, started pseudocode

---
+++ script.js
@@ -0,0 +1,57 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+/* Starting Pseudo-code
+Prompt for password criteria
+
+Prompt for length of password
+  choose a length of at least 8 characters and no more than 128 characters
+  If answer is not between 8 and 128 ALERT that answer must be min 8 max 128
+
+Prompt to include special characters
+Prompt to include numeric charcters
+Prompt to include lowercase characters
+Prompt to include uppercase characters
+
+Generate password
+
+
+
+Acceptance Criteria
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+
+
+
+
+
+
+
+*/
